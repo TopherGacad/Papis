9b9004bebc904a96553908d5c08385c99b307162
Christopher Gacad <124163444+TopherGacad@users.noreply.github.com>
2024-06-11 13:48:10 +0800

    Revert "6 create login page"

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-// import { router } from "./Router";
-import Sidebar from "./components/Sidebar";
-import Content from "./components/Content";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-// ROUTER
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Router";
+function App() {
+  const [count, setCount] = useState(0)
 
-const App = () => {
   return (
-    <>
-      <RouterProvider router={router} />
-      {/* <Navbar></Navbar> */}
-      {/* <div className="flex">
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
 
-      </div> */}
-    </>
-  );
-};
-
-export default App;
+export default App
