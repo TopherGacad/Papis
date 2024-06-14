5559bf50da43d1de2d30f223eebce7b5fe1ada49
TopherGacad <gacadtophs@gmail.com>
2024-06-14 17:54:53 +0800

    Feat: cursor-pointer for all buttons in content page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -24,13 +24,13 @@ export default function Sidebar() {
           <div className="head-left flex items-center">
             <span className="w-[50px] h-[50px] rounded-[50%] border-solid border-[1px] border-black-600 mx-2"></span>
             <span className="text-base font-bold items-center">Chats</span>
-            <FaSortDown className="w-[25px] h-[25px] pl-[5px] mb-[10px]"></FaSortDown>
+            <FaSortDown className="w-[25px] h-[25px] pl-[5px] mb-[10px] cursor-pointer"></FaSortDown>
           </div>
           <div className="head-right flex justify-around items-center">
             <span className="pr-[10px]">
-              <TbSettings2 className=" w-[25px] h-[25px]"></TbSettings2>
+              <TbSettings2 className=" w-[25px] h-[25px] cursor-pointer"></TbSettings2>
             </span>
-            <IoAddCircleSharp className="w-[25px] h-[25px]"></IoAddCircleSharp>
+            <IoAddCircleSharp className="w-[25px] h-[25px] cursor-pointer"></IoAddCircleSharp>
           </div>
         </div>  
 
